78b2f2c7ad821259639f24436670dd56f218d9f4
Glitch (draughts-game) <none>
2020-03-18 17:08:03 +0000

    🌄🌑 Checkpoint ./config/auth.js:11995829/376

--- config/auth.js
+++ config/auth.js
@@ -1,3 +1,8 @@
+/*
+dans cette partie on permet à la fonction ensureAuthenticated et forwardAuthenticated d'etre visible dans les autres 
+packages en gros ces deux fonctions assurent que l'utilisateur s'est authentifié avant d'acceder aux ressources 
+
+*/
 module.exports = {
   ensureAuthenticated: function(req, res, next) {
     if (req.isAuthenticated()) {
